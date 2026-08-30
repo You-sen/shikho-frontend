558078ae20528c4f2ef69698c807bf1d3830c0cc
You-sen <88145204+You-sen@users.noreply.github.com>
2026-08-30 13:59:42 +0600

    nav added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { AuthProvider } from '@/lib/AuthContext';
 import "./globals.css";
+import Nav from "@/components/nav";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,7 +34,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body>
         <AuthProvider>
-          {children}</AuthProvider>
+          <Nav/>
+          {children}
+          </AuthProvider>
       </body>
     </html>
   );
